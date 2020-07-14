e7cf439cb942703e9592bad78bf5ea5f799ad584
fumiaki49 <me.fk39@gmail.com>
2020-07-14 13:15:27 +0900

    create webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const MODE = "production";
+const enabledSourceMap = MODE === "development";
+
+module.exports = {
+  mode: MODE,
+  watch: true,
+  module: {
+    rules: [
+      // Sassファイルの読み込みとコンパイル
+      {
+        test: /\.scss/, // 対象となるファイルの拡張子
+        use: [
+          // linkタグに出力する機能
+          "style-loader",
+          // CSSをバンドルするための機能
+          {
+            loader: "css-loader",
+            options: {
+              // オプションでCSS内のurl()メソッドの取り込みを禁止する
+              url: false,
+              // ソースマップの利用有無
+              sourceMap: enabledSourceMap,
+
+              // 0 => no loaders (default);
+              // 1 => postcss-loader;
+              // 2 => postcss-loader, sass-loader
+              importLoaders: 2
+            }
+          },
+          {
+            loader: "sass-loader",
+            options: {
+              // ソースマップの利用有無
+              sourceMap: enabledSourceMap
+            }
+          }
+        ]
+      }
+    ]
+  }
+};
