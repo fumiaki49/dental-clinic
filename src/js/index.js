d4988b06a0c95a651d4433c8e96b27a68260fa29
fumiaki49 <me.fk39@gmail.com>
2020-07-18 16:22:05 +0900

    ページの共通部分をテンプレート化

---
+++ src/js/index.js
@@ -0,0 +1,9 @@
+import{headerTemplate} from "./modules/template";
+import{menuTemplate} from "./modules/template";
+import{footerTemplate} from "./modules/template";
+
+$(function () {
+  headerTemplate();
+  menuTemplate();
+  footerTemplate();
+});
